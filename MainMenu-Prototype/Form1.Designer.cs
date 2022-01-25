7ea5546ea53e5e68cf53144c548a8270f15af619
Aleks <80444715+sashimaru999@users.noreply.github.com>
2022-01-24 18:33:15 -0800

    Addded text changes

--- MainMenu-Prototype/Form1.Designer.cs
+++ MainMenu-Prototype/Form1.Designer.cs
@@ -56,9 +56,8 @@ namespace MainMenu_Prototype
             this.leftMainPanel.Controls.Add(this.logoPanel);
             this.leftMainPanel.Dock = System.Windows.Forms.DockStyle.Left;
             this.leftMainPanel.Location = new System.Drawing.Point(0, 0);
-            this.leftMainPanel.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.leftMainPanel.Name = "leftMainPanel";
-            this.leftMainPanel.Size = new System.Drawing.Size(400, 865);
+            this.leftMainPanel.Size = new System.Drawing.Size(200, 450);
             this.leftMainPanel.TabIndex = 0;
             // 
             // tagPanel
@@ -72,10 +71,9 @@ namespace MainMenu_Prototype
             this.tagPanel.Controls.Add(this.subTagButton1);
             this.tagPanel.Controls.Add(this.tagButton);
             this.tagPanel.Dock = System.Windows.Forms.DockStyle.Top;
-            this.tagPanel.Location = new System.Drawing.Point(0, 154);
-            this.tagPanel.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.tagPanel.Location = new System.Drawing.Point(0, 91);
             this.tagPanel.Name = "tagPanel";
-            this.tagPanel.Size = new System.Drawing.Size(400, 469);
+            this.tagPanel.Size = new System.Drawing.Size(200, 244);
             this.tagPanel.TabIndex = 2;
             // 
             // addTagButton
@@ -86,11 +84,10 @@ namespace MainMenu_Prototype
             this.addTagButton.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.addTagButton.Image = ((System.Drawing.Image)(resources.GetObject("addTagButton.Image")));
             this.addTagButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.addTagButton.Location = new System.Drawing.Point(0, 393);
-            this.addTagButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.addTagButton.Location = new System.Drawing.Point(0, 205);
             this.addTagButton.Name = "addTagButton";
-            this.addTagButton.Padding = new System.Windows.Forms.Padding(50, 0, 40, 0);
-            this.addTagButton.Size = new System.Drawing.Size(400, 58);
+            this.addTagButton.Padding = new System.Windows.Forms.Padding(25, 0, 20, 0);
+            this.addTagButton.Size = new System.Drawing.Size(200, 39);
             this.addTagButton.TabIndex = 6;
             this.addTagButton.Text = "Add Tag";
             this.addTagButton.UseVisualStyleBackColor = true;
@@ -102,12 +99,11 @@ namespace MainMenu_Prototype
             this.button5.FlatAppearance.BorderSize = 0;
             this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button5.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(0, 326);
-            this.button5.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button5.Location = new System.Drawing.Point(0, 170);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(400, 67);
+            this.button5.Size = new System.Drawing.Size(200, 35);
             this.button5.TabIndex = 5;
-            this.button5.Text = "Favorites";
+            this.button5.Text = "Category2";
             this.button5.UseVisualStyleBackColor = false;
             // 
             // button4
@@ -117,12 +113,11 @@ namespace MainMenu_Prototype
             this.button4.FlatAppearance.BorderSize = 0;
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(0, 259);
-            this.button4.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button4.Location = new System.Drawing.Point(0, 135);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(400, 67);
+            this.button4.Size = new System.Drawing.Size(200, 35);
             this.button4.TabIndex = 4;
-            this.button4.Text = "Favorites";
+            this.button4.Text = "Favorite";
             this.button4.UseVisualStyleBackColor = false;
             // 
             // button3
@@ -132,12 +127,11 @@ namespace MainMenu_Prototype
             this.button3.FlatAppearance.BorderSize = 0;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(0, 192);
-            this.button3.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button3.Location = new System.Drawing.Point(0, 100);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(400, 67);
+            this.button3.Size = new System.Drawing.Size(200, 35);
             this.button3.TabIndex = 3;
-            this.button3.Text = "Favorites";
+            this.button3.Text = "Shopping";
             this.button3.UseVisualStyleBackColor = false;
             // 
             // button2
@@ -147,12 +141,11 @@ namespace MainMenu_Prototype
             this.button2.FlatAppearance.BorderSize = 0;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(0, 125);
-            this.button2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button2.Location = new System.Drawing.Point(0, 65);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(400, 67);
+            this.button2.Size = new System.Drawing.Size(200, 35);
             this.button2.TabIndex = 2;
-            this.button2.Text = "Favorites";
+            this.button2.Text = "Social";
             this.button2.UseVisualStyleBackColor = false;
             // 
             // subTagButton1
@@ -162,12 +155,11 @@ namespace MainMenu_Prototype
             this.subTagButton1.FlatAppearance.BorderSize = 0;
             this.subTagButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.subTagButton1.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.subTagButton1.Location = new System.Drawing.Point(0, 58);
-            this.subTagButton1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.subTagButton1.Location = new System.Drawing.Point(0, 30);
             this.subTagButton1.Name = "subTagButton1";
-            this.subTagButton1.Size = new System.Drawing.Size(400, 67);
+            this.subTagButton1.Size = new System.Drawing.Size(200, 35);
             this.subTagButton1.TabIndex = 1;
-            this.subTagButton1.Text = "Favorites";
+            this.subTagButton1.Text = "Email";
             this.subTagButton1.UseVisualStyleBackColor = false;
             // 
             // tagButton
@@ -176,13 +168,12 @@ namespace MainMenu_Prototype
             this.tagButton.Dock = System.Windows.Forms.DockStyle.Top;
             this.tagButton.FlatAppearance.BorderSize = 0;
             this.tagButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.tagButton.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tagButton.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tagButton.Location = new System.Drawing.Point(0, 0);
-            this.tagButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.tagButton.Name = "tagButton";
-            this.tagButton.Size = new System.Drawing.Size(400, 58);
+            this.tagButton.Size = new System.Drawing.Size(200, 30);
             this.tagButton.TabIndex = 0;
-            this.tagButton.Text = "Tags";
+            this.tagButton.Text = "Categories";
             this.tagButton.TextAlign = System.Drawing.ContentAlignment.TopLeft;
             this.tagButton.UseVisualStyleBackColor = false;
             // 
@@ -192,11 +183,10 @@ namespace MainMenu_Prototype
             this.button1.Dock = System.Windows.Forms.DockStyle.Top;
             this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(0, 96);
-            this.button1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button1.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(0, 50);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(400, 58);
+            this.button1.Size = new System.Drawing.Size(200, 41);
             this.button1.TabIndex = 1;
             this.button1.Text = "My Vault";
             this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -207,35 +197,35 @@ namespace MainMenu_Prototype
             this.logoPanel.BackColor = System.Drawing.Color.LightSeaGreen;
             this.logoPanel.Dock = System.Windows.Forms.DockStyle.Top;
             this.logoPanel.Location = new System.Drawing.Point(0, 0);
-            this.logoPanel.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.logoPanel.Name = "logoPanel";
-            this.logoPanel.Size = new System.Drawing.Size(400, 96);
+            this.logoPanel.Size = new System.Drawing.Size(200, 50);
             this.logoPanel.TabIndex = 0;
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.textBox1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(400, 0);
+            this.panel1.Location = new System.Drawing.Point(200, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1200, 100);
+            this.panel1.Size = new System.Drawing.Size(600, 52);
             this.panel1.TabIndex = 1;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(79, 40);
+            this.textBox1.Location = new System.Drawing.Point(40, 21);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(551, 31);
+            this.textBox1.Size = new System.Drawing.Size(278, 20);
             this.textBox1.TabIndex = 0;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1600, 865);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.leftMainPanel);
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form1";
             this.Text = "Form1";
             this.leftMainPanel.ResumeLayout(false);
