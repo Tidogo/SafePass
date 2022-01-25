a1fed59de6ac8057e6718650671c3d7d19ae150a
tdattran98 <tdattran98@gmail.com>
2022-01-25 01:54:47 -0800

    added data grid prototype #1

--- MainMenu-Prototype/Form1.cs
+++ MainMenu-Prototype/Form1.cs
@@ -10,16 +10,28 @@ using System.Windows.Forms;
 
 namespace MainMenu_Prototype
 {
+
     public partial class Form1 : Form
     {
-        TextBox searchTextBox = new TextBox();
         
         public Form1()
         {
             InitializeComponent();
+            
         }
 
-
-        
+        //Event handler for search box in main menu
+        //Click on searchbox will remove "Search Vault"
+        //to allow new entry.
+        private void searchBox_Click(object sender, EventArgs e)
+        {
+            if (searchBox.Text == "Search Vault")
+            {
+                searchBox.Text = " ";
+            } else if (string.IsNullOrEmpty(searchBox.Text))
+            {
+                searchBox.Text = "Search Vault";
+            }
+        }
     }
 }
