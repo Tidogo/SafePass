82a7e899439c63ce591ad5227a1ad25ee9a4d6a0
Timothy Goddard <jediknightjaden@msn.com>
2022-01-30 14:58:06 -0800

    Added connection string to program.cs

--- SignUp/Program.cs
+++ SignUp/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Text;
 
 namespace SafePass_Application
 {
@@ -14,6 +16,39 @@ namespace SafePass_Application
         [STAThread]
         static void Main()
         {
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "safepass-serv.database.windows.net";
+                builder.UserID = "db-admin";
+                builder.Password = "af8kK$T7Da";
+                builder.InitialCatalog = "safepass-db";
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    Console.WriteLine("\nQuery data example:");
+                    Console.WriteLine("=========================================\n");
+
+                    String sql = "SELECT name, collation_name FROM sys.databases";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        connection.Open();
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.ReadLine();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmLogin());
